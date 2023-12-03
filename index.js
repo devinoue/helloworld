65ad421fd6d6b1ca07445eaa1028d4ceae6280d0
Inoue <itisagoodcat@gmail.com>
2023-12-04 04:09:24 +0900

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   const name = process.env.NAME || "World";
-  res.send(`Hello ${name}!`);
+  res.send(`Update! Hello ${name}!`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
